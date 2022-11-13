c0c4ccb72fa19f133430da75de7c18a4548949cb
Lightening Gibson <lighteningibson@gmail.com>
2022-11-13 13:45:18 -0600

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+          const navBarLinks = links.map((link) => {
+            return <a key={link} href={"#" + link}>{link}</a>
+          })
+  return (  <nav>
+
+              {navBarLinks}
+
+
+
+          </nav>);
 }
 
 export default NavBar;
