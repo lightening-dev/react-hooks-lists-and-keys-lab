2a7690eaec90f4710b9934b9b03abdf5de304e6e
Lightening Gibson <lighteningibson@gmail.com>
2022-11-13 13:44:57 -0600

    commit tree 7eea1f59c968efd3085c6e4d20f04fc679af69d1

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,12 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const project = projects.map((eachProject) => <ProjectItem key={eachProject.id} name ={eachProject.name} technologies={eachProject.technologies}/>)
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{project}</div>
     </div>
   );
 }
